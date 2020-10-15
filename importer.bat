d690932b9d39d5315194c1df31b088b376766555
Hamill <1544152390121002@mil>
2020-10-15 13:50:45 -0400

    Example for Jeremy G

--- importer.bat
+++ importer.bat
@@ -1,4 +1,4 @@
-set "VORTEX_HOME=C:\workspace\git_clones\vortex-0.9.3-rc.2"
+set "VORTEX_HOME=C:\workspace\git_clones\vortex-0.10.11"
 set "PATH=%VORTEX_HOME%\bin;%VORTEX_HOME%\bin\gdal;%PATH%"
 set "GDAL_DRIVER_PATH=%VORTEX_HOME%\bin\gdal\gdalplugins"
 set "GDAL_DATA=%VORTEX_HOME%\bin\gdal\gdal-data"
@@ -6,4 +6,5 @@ set "PROJ_LIB=%VORTEX_HOME%\bin\gdal\projlib"
 set "CLASSPATH=%VORTEX_HOME%\lib\*"
 
 
-C:\jython2.7.2\bin\jython.exe -Djava.library.path=%VORTEX_HOME%\bin;%VORTEX_HOME%\bin\gdal C:\Users\RDCRLDDH\Desktop\IMPORTER_CLIP.py
+C:\jython2.7.2\bin\jython.exe -Djava.library.path=%VORTEX_HOME%\bin;%VORTEX_HOME%\bin\gdal C:\workspace\git_clones\vortex_scripting\importer.py
+cmd /k
--- importer.py
+++ importer.py
@@ -8,25 +8,25 @@ from mil.army.usace.hec.vortex.io import BatchImporter
 from mil.army.usace.hec.vortex import Options
 from mil.army.usace.hec.vortex.geo import WktFactory
 from glob import glob
-in_files = glob(r"E:\Ririe_RTI_M\snodas\*_WC.dss")
 
-
-clip_shp = r"E:\ririe\hms\GIS\Willow_Creek_Subbasin.shp"
+in_files = glob(r"G:\PRISM\WY_2016_PRISM\*.bil")
+variables = ['ppt']
+clip_shp = r"C:\workspace\Post-Wildfire\shp\Watersheds\poudre_alb.shp"
 
 geo_options = Options.create()
 geo_options.add('pathToShp', clip_shp)
 geo_options.add('targetCellSize', '2000')
 geo_options.add('targetWkt', WktFactory.shg())
 
-destination = r"E:\Ririe_RTI_M\snodas\clipped\RIRIE_IMPORTED_SNODAS.dss"
+destination = r"C:\workspace\Post-Wildfire\dss\PRISM\POUDRE_WY16_PRISM.dss"
 
 write_options = Options.create()
-write_options.add('partF', 'SNODAAS')
+write_options.add('partF', 'PRISM')
 write_options.add('partA', 'SHG')
-write_options.add('partF', 'WILLOW CREEK')
+write_options.add('partB', 'CACHE LA POUDRE')
 myImport = BatchImporter.builder() \
     .inFiles(in_files) \
-    .selectAllVariables() \
+    .variables(variables) \
     .geoOptions(geo_options) \
     .destination(destination) \
     .writeOptions(write_options) \
